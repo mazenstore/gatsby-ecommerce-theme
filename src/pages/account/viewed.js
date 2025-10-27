ff04a713b538bbaba82f76854c57d9d28176139b
mazenstore <mazenshaban221@gmail.com>
2025-10-27 20:22:23 +0300

    Update viewed.js

--- src/pages/account/viewed.js
+++ src/pages/account/viewed.js
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function Viewed() {
+  return (
+    <div>
+      <h1>Viewed Page</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
