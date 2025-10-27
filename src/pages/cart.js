6bb5621788951c761e68c445160f1a474ee6c144
mazenstore <mazenshaban221@gmail.com>
2025-10-27 19:48:20 +0300

    Update cart.js

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,57 +1,18 @@
-import { Link } from 'gatsby';
 import React from 'react';
-
-import Brand from '../components/Brand';
-import CartItem from '../components/CartItem';
 import Container from '../components/Container';
 import Footer from '../components/Footer';
-import Icon from '../components/Icons/Icon';
-import OrderSummary from '../components/OrderSummary';
+import Brand from '../components/Brand';
 
-import * as styles from './cart.module.css';
-
-const CartPage = (props) => {
-  const sampleCartItem = {
-    image: '/products/pdp1.jpeg',
-    alt: '',
-    name: 'Lambswool Crew Neck Jumper',
-    price: 220,
-    color: 'Anthracite Melange',
-    size: 'XS',
-  };
-
+const CartPage = () => {
   return (
-    <div>
-      <div className={styles.contentContainer}>
-        <Container size={'large'} spacing={'min'}>
-          <div className={styles.headerContainer}>
-            <div className={styles.shoppingContainer}>
-              <Link className={styles.shopLink} to={'/shop'}>
-                <Icon symbol={'arrow'}></Icon>
-                <span className={styles.continueShopping}>
-                  Continue Shopping
-                </span>
-              </Link>
-            </div>
-            <Brand />
-            <div className={styles.loginContainer}>
-              <Link to={'/login'}>Login</Link>
-            </div>
-          </div>
-          <div className={styles.summaryContainer}>
-            <h3>My Bag</h3>
-            <div className={styles.cartContainer}>
-              <div className={styles.cartItemsContainer}>
-                <CartItem {...sampleCartItem} />
-                <CartItem {...sampleCartItem} />
-              </div>
-              <OrderSummary />
-            </div>
-          </div>
-        </Container>
+    <Container>
+      <Brand />
+      <div style={{ padding: '50px 0', textAlign: 'center' }}>
+        <h2>🛒 سلة المشتريات</h2>
+        <p>لم يتم تفعيل صفحة السلة بعد.</p>
       </div>
       <Footer />
-    </div>
+    </Container>
   );
 };
 
